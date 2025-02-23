81794700763376151ab9b6db029ccb832546e558
최경영 <KyeongYeongDEV>
2025-02-23 18:57:35 +0900

    FEAT : user Validator 추가 (pipe, dto)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // DTO 자동 변환
 
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
@@ -11,6 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('users')
     .build();
+    
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -10,9 +11,23 @@ export class UserService {
     return this.prisma.user.findMany();
   }
 
-  async createUser(email: string, name?: string): Promise<User> {
+  async getUserById(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async createUser( { email, name } : CreateUserDto): Promise<User> {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new Error('이미 존재하는 이메일입니다.');
+    }
+
     return this.prisma.user.create({
-      data: { email, name },
+      data: {email, name},
     });
   }
 }
